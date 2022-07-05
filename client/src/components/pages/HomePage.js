08ae0fa38dc8cd46b04830f146ea4ec2975bc258
Ali Jahankah <aral2@yahoo.com>
2022-07-06 00:51:42 +0100

    fix the bugs

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 const HomePage = () => {
   const [test, setTest] = useState("");
   useEffect(() => {
-    const url = "https://cyf-devquad-edu-app.herokuapp.com/api/user";
+    const url = "http://localhost:5000/api/user";
     const callServer = async () => {
       const res = await fetch(url);
       const { message } = await res.json();
