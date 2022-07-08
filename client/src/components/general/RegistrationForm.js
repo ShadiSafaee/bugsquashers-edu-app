fb1fbfd22991077d0a1119a5fdc8c5598fe5da38
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-07-08 23:12:35 +0100

    forms amended

--- client/src/components/general/RegistrationForm.js
+++ client/src/components/general/RegistrationForm.js
@@ -1,67 +1,38 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import "../styles/RegistrationForm.css";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Select from "react-select";
-import countryList from "react-select-country-list";
+import { Link } from "react-router-dom";
 
 const RegistrationForm = () => {
-  const [username, setUsername] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirm, setConfirm] = useState(null);
-  const [name, setName] = useState(null);
+  const [registrationData, setRegistrationData] = useState({
+    email: "",
+    password: "",
+    confirm: "",
+    dob: "",
+  });
   const [selectedDate, setSelectedDate] = useState(null);
-  const [value, setValue] = useState("");
 
-  const options = useMemo(() => countryList().getData(), []);
-
-  const changeHandler = (e) => {
-    setValue(e);
+  const handleChange = (key, value) => {
+    setRegistrationData({ ...registrationData, [key]: value });
   };
-
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-
-    if (id === "username") {
-      setUsername(value);
-    }
-    if (id === "email") {
-      setEmail(value);
-    }
-    if (id === "password") {
-      setPassword(value);
-    }
-    if (id === "confirm") {
-      setConfirm(value);
-    }
-    if (id === "name") {
-      setName(value);
-    }
-  };
+  const countries = [
+    "Mexico",
+    "Colombia",
+    "Bolivia",
+    "Dominican Republic",
+    "South Africa",
+    "Lebanon",
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className="form">
+    <section className="form">
       <h2>Register</h2>
-      <div className="form_body">
-        <div className="username">
-          <label className="form_label" for="username">
-            Username
-          </label>
-          <input
-            className="form_input"
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => handleInputChange(e)}
-            placeholder="Username..."
-            required
-          ></input>
-        </div>
+      <form className="form_body">
         <div className="email">
           <label className="form_label" for="email">
             Email
@@ -69,9 +40,8 @@ const RegistrationForm = () => {
           <input
             className="form_input"
             type="email"
-            id="email"
-            value={email}
-            onChange={(e) => handleInputChange(e)}
+            value={registrationData.email}
+            onChange={(e) => handleChange("email", e.target.value)}
             placeholder="Email..."
             required
           ></input>
@@ -83,38 +53,23 @@ const RegistrationForm = () => {
           <input
             className="form_input"
             type="text"
-            id="password"
-            value={password}
-            onChange={(e) => handleInputChange(e)}
+            value={registrationData.password}
+            onChange={(e) => handleChange("password", e.target.value)}
             placeholder="Password..."
             required
           ></input>
         </div>
         <div className="confirm_password">
           <label className="form_label" for="confirm">
-            Confirm
+            Confirm Password
           </label>
           <input
             className="form_input"
             type="text"
-            id="confirm"
-            value={confirm}
-            onChange={(e) => handleInputChange(e)}
+            value={registrationData.confirm}
+            onChange={(e) => handleChange("confirm", e.target.value)}
             placeholder="Confirm password..."
             required
-          ></input>
-        </div>
-        <div className="name">
-          <label className="form_label" for="name">
-            Name
-          </label>
-          <input
-            className="form_input"
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => handleInputChange(e)}
-            placeholder="Name..."
           ></input>
         </div>
         <div>
@@ -127,25 +82,27 @@ const RegistrationForm = () => {
             onChange={(date) => setSelectedDate(date)}
           />
         </div>
+
         <div>
           <label className="country" for="country">
             Country
           </label>
-          <Select
-            className="select"
-            options={options}
-            value={value}
-            onChange={changeHandler}
-          />
+          <select>
+            {countries.map((country) => (
+              <option>{country}</option>
+            ))}
+          </select>
         </div>
-      </div>
+      </form>
 
       <div className="form_button">
         <button onSubmit={() => handleSubmit()} type="submit" className="btn">
           Register
         </button>
       </div>
-    </div>
+
+      {/* <Link to="/login">Already Registered?</Link> */}
+    </section>
   );
 };
 
