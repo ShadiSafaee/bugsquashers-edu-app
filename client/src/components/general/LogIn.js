c9589e284a71dd669726162cfea45326737dbec4
Ali Jahankah <aral2@yahoo.com>
2022-07-09 09:12:13 +0100

    fixing kate branch

--- client/src/components/general/LogIn.js
+++ client/src/components/general/LogIn.js
@@ -3,24 +3,26 @@ import { React } from "react";
 
 const LogIn = () => {
   return (
-    <section className="log">
+    <section className="login_section">
       <h2>Log In</h2>
-      <form className="log_body">
+      <form className="login_form">
         <input
-          className="log_input"
+          className="login_input"
           type="email"
           placeholder="Email..."
           required
         />
         <input
-          className="log_input"
+          className="login_input"
           type="password"
           placeholder="Password..."
           required
         />
-        <button className="log_btn">Log In</button>
+        <button className="log_btn" type="submit">
+          Log In
+        </button>
       </form>
-      {/* <Link to="/???">Forgot Password?</Link> */}
+      {/* <Link to="/???">Forgot Password? Reset your password Here</Link> */}
     </section>
   );
 };
