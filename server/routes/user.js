3411950864d4e7accd58aa1b68f870ee3775334d
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-07-11 16:44:07 +0100

    fix the logout

--- server/routes/user.js
+++ server/routes/user.js
@@ -28,12 +28,12 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-  let { firstname, surname, email, password, dob, country } = req.body;
+  let { firstname, surname, email, password, dob, country, role } = req.body;
   const signUpQuery =
-    "INSERT INTO user_data (firstname, surname, email, password, dob, country) VALUES ($1, $2, $3, $4, $5, $6)";
+    "INSERT INTO user_data (firstname, surname, email, password, dob, country, role) VALUES ($1, $2, $3, $4, $5, $6, $7)";
   const checkEmailQuery =
     "SELECT EXISTS (SELECT email FROM user_data WHERE email= $1)";
-  const query = "SELECT * FROM user_data";
+  const query = "SELECT * FROM user_data WHERE email = $1";
 
   const emailChecked = await pool.query(checkEmailQuery, [email]);
 
@@ -56,8 +56,9 @@ router.post("/signup", async (req, res) => {
       password,
       dob,
       country,
+      role,
     ]);
-    const data = await pool.query(query);
+    const data = await pool.query(query, [email]);
     res.status(200).json({ msg: "New user created", user: data.rows });
   }
 });
@@ -95,6 +96,7 @@ router.post("/login", async (req, res) => {
   } else {
     res.status(400).json({ msg: "user not found" });
   }
+  // res.redirect("/dashboard");
 });
 
 //Check to make sure header is not undefined, if so, return Forbidden (403), checkToken variable is being initialized here so that we can use it for the get req at the next level
@@ -114,7 +116,6 @@ const checkToken = (req, res, next) => {
 
 //accessing a protected route for user
 router.post("/dashboard", checkToken, (req, res) => {
-  console.log(req.token, process.env.mySecret);
   jwt.verify(req.token, process.env.mySecret, (err, authorizedData) => {
     if (err) {
       res.status(403).json({ msg: "Not authorized" });
@@ -123,7 +124,23 @@ router.post("/dashboard", checkToken, (req, res) => {
     }
   });
 });
-// this is a test
-// second test
+
+//log out route
+router.put("/logout", checkToken, (req, res) => {
+  const authHeader = req.headers["authorization"];
+  process.env.mySecret = crypto.randomBytes(64).toString("hex");
+  jwt.sign(
+    authHeader,
+    process.env.mySecret,
+    { expiresIn: 1 },
+    (logout, err) => {
+      if (logout) {
+        res.send({ msg: "You have been Logged Out" });
+      } else {
+        res.send({ msg: "Error" });
+      }
+    }
+  );
+});
 
 module.exports = router;
