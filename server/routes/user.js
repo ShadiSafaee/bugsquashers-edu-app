7c1516759e7c8bfbe78186040087d447c1377e8f
Ali Jahankah <aral2@yahoo.com>
2022-07-12 00:11:12 +0100

    add express

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const app = express();
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
