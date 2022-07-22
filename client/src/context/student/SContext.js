792abc84035aafd04047e7c6d5f36f2b970464b3
Ali Jahankah <aral2@yahoo.com>
2022-07-22 20:39:37 +0100

    merging

--- client/src/context/student/SContext.js
+++ client/src/context/student/SContext.js
@@ -4,6 +4,8 @@ import StudentContext from "./StudentContext";
 const SContext = ({ children }) => {
   const [modules, setModules] = useState([]);
   const [lessons, setLessons] = useState([]);
+  const [lesson, setLesson] = useState({});
+  const [submitFile, setSubmitFile] = useState({});
   const getModulesHandler = async () => {
     const url = "http://localhost:5000/api/teacher/modules";
     const res = await fetch(url);
@@ -41,6 +43,39 @@ const SContext = ({ children }) => {
       console.log(error);
     }
   };
+  const getLessonHandler = async (id) => {
+    const url = `http://localhost:5000/api/teacher/lesson/${id}`;
+    if (id) {
+      const res = await fetch(url);
+      if (res.ok) {
+        const data = await res.json();
+        setLesson(data.data);
+      } else {
+        alert("Lesson not found");
+      }
+    }
+  };
+  const submitLessonHandler = async (userId, lessonId) => {
+    const myBody = { user_id: userId, lesson_id: lessonId, submitFile };
+    const postOption = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myBody),
+    };
+    const url = "http://localhost:5000/api/teacher/addnewsubmission";
+    try {
+      const res = fetch(url, postOption);
+      if (res.ok) {
+        alert("Lesson Submitted!");
+      } else {
+        alert(res.msg);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <StudentContext.Provider
@@ -51,6 +86,12 @@ const SContext = ({ children }) => {
         moduleHandler,
         lessons,
         setLessons,
+        getLessonHandler,
+        lesson,
+        setLesson,
+        submitFile,
+        setSubmitFile,
+        submitLessonHandler,
       }}
     >
       {children}
