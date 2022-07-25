8747bbc9eeb9d798507fce7d942a15adb293296f
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-07-25 18:29:55 +0100

    tables amended

--- server/database.sql
+++ server/database.sql
@@ -1,8 +1,7 @@
-DROP TABLE IF EXISTS modules;
+DROP TABLE IF EXISTS submission;
 DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS modules;
 DROP TABLE IF EXISTS user_data;
-
-
 CREATE TABLE user_data (
     id SERIAL PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
@@ -13,8 +12,6 @@ CREATE TABLE user_data (
     country VARCHAR(50) NOT NULL,
     role VARCHAR(10) NOT NULL
 );
-
-
 CREATE TABLE modules (
     id SERIAL PRIMARY KEY,
     module_name VARCHAR(100) NOT NULL,
@@ -23,18 +20,16 @@ CREATE TABLE modules (
 );
 CREATE TABLE lessons (
     id SERIAL PRIMARY KEY,
-    module_id INT NOT NULL REFERENCES modules(id),
+    module_id INT NOT NULL REFERENCES modules(id) ON DELETE CASCADE,
     lesson_name VARCHAR(200) NOT NULL,
     lesson_description VARCHAR(6000),
     lesson_type VARCHAR(50) NOT NULL,
     lesson_url VARCHAR(100),
     lesson_created_date VARCHAR(20) NOT NULL
 );
-
-
 CREATE TABLE submission (
     id SERIAL PRIMARY KEY,
-    lesson_id INT NOT NULL REFERENCES lessons(id),
+    lesson_id INT NOT NULL REFERENCES lessons(id) ON DELETE CASCADE,
     user_id INT NOT NULL REFERENCES user_data(id),
     comment VARCHAR(8000),
     created_date Date NOT NULL DEFAULT CURRENT_DATE,
@@ -44,5 +39,3 @@ CREATE TABLE submission (
     mark_by INT REFERENCES user_data(id),
     mark_comments VARCHAR(8000) DEFAULT NULL
 );
-
-
