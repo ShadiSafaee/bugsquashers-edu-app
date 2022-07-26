0a9e2042a57f86ecb5fc24b7623484e46c262578
Ali Jahankah <aral2@yahoo.com>
2022-07-26 21:18:55 +0100

    fixing

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, path.join(__dirname, "/uploads/"));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "--" + file.originalname);
