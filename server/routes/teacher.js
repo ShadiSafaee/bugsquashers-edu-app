f926a5913e6a71ca4aa787e1514ed77c941ac9a6
Ali Jahankah <aral2@yahoo.com>
2022-07-26 20:56:20 +0100

    fixing

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { Pool } = require("pg");
 const multer = require("multer");
-const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "/uploads/"));
+    cb(null, __dirname);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "--" + file.originalname);
+    cb(null, new Date().toISOString() + "--" + file.originalname);
   },
 });
 let upload = multer({ storage: storage });
